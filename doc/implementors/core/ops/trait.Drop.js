83683b8dc9f065e349e79d8e9d0b79d790ed7b3f
John Shaw <jshaw@shawnation.com>
2016-03-20 15:03:18 -0500

    Fix docs

---
+++ doc/implementors/core/ops/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['magnetic'] = ["impl&lt;T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='magnetic/buffer/dynamic/struct.DynamicBuffer.html' title='magnetic::buffer::dynamic::DynamicBuffer'>DynamicBuffer</a>&lt;T&gt;","impl&lt;T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='magnetic/buffer/dynamic/struct.DynamicBufferP2.html' title='magnetic::buffer::dynamic::DynamicBufferP2'>DynamicBufferP2</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
